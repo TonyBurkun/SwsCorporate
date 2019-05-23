1b07ffac2690653686f931b008e606a21863da9e
Anton <burkun.anton@gmail.com>
2019-05-23 15:46:33 +0300

    added simple validation for the feedback form

--- my-app/src/reuse/contact/contact.component.js
+++ my-app/src/reuse/contact/contact.component.js
@@ -5,25 +5,107 @@ class ContactComponent extends Component {
     constructor(props) {
         super(props);
 
+        this.state={
+            name: '',
+            email: '',
+            phone: '',
+            message: '',
+            formValid: false,
+
+        };
+
         this.sendForm = this.sendForm.bind(this);
+
     }
 
     sendForm(e) {
         e.preventDefault();
-        const data = new FormData(e.target);
-        console.log('Your favorite flavor is: ' + JSON.stringify(this.state));
-        fetch('http://cp.stairwaysoft.com/api/contact-form-7/v1/contact-forms/407/feedback',
-            {
-                method: 'POST',
-                body: data
-            })
-            .then(response => response.json())
-            .then( response => {
-                console.log(response);
-            });
+
+        this.setState({
+            name: this.state.name.trim(),
+            email: this.state.email.trim(),
+            phone: this.state.phone.trim(),
+            message: this.state.message.trim(),
+        });
+
+        let isEmailValid = false,
+            isNameValid = false,
+            isMessageValid = false;
+
+
+        const {name, email, message} = this.state;
+        const emailRegular = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        const feedbackNameInput = document.getElementById('feedback-name');
+        const feedbackNameLabel = feedbackNameInput.closest('label');
+
+        const feedbackEmailInput = document.getElementById('feedback-email');
+        const feedbackEmailLabel = feedbackEmailInput.closest('label');
+
+        const feedbackMessageInput = document.getElementById('feedback-message');
+        const feedbackMessageLabel = feedbackMessageInput.closest('label');
+
+
+        if (!name.trim().length) {
+            feedbackNameLabel.classList.add('feedback-form__label--error');
+            feedbackNameInput.classList.add('feedback-form__input--error');
+            isNameValid = false;
+        } else {
+            feedbackNameLabel.classList.remove('feedback-form__label--error');
+            feedbackNameInput.classList.remove('feedback-form__input--error');
+            isNameValid = true;
+        }
+
+        if (!email.trim().length) {
+            feedbackEmailLabel.classList.add('feedback-form__label--error');
+            feedbackEmailInput.classList.add('feedback-form__input--error');
+            isEmailValid = false;
+        } else {
+            const result = emailRegular.test(String(email).toLowerCase());
+
+            if (!result) {
+                feedbackEmailLabel.classList.add('feedback-form__label--error');
+                feedbackEmailInput.classList.add('feedback-form__input--error');
+                isEmailValid = false;
+            } else {
+                feedbackEmailLabel.classList.remove('feedback-form__label--error');
+                feedbackEmailInput.classList.remove('feedback-form__input--error');
+                isEmailValid = true;
+            }
+        }
+
+        if (!message.trim().length) {
+            feedbackMessageLabel.classList.add('feedback-form__label--error');
+            feedbackMessageInput.classList.add('feedback-form__input--error');
+            isMessageValid = false;
+        } else {
+            feedbackMessageLabel.classList.remove('feedback-form__label--error');
+            feedbackMessageInput.classList.remove('feedback-form__input--error');
+            isMessageValid = true;
+        }
+
+
+
+        if (isNameValid && isEmailValid && isMessageValid) {
+            const data = new FormData(e.target);
+
+            console.log('Your favorite flavor is: ' + JSON.stringify(this.state));
+            fetch('http://cp.stairwaysoft.com/api/contact-form-7/v1/contact-forms/407/feedback',
+                {
+                    method: 'POST',
+                    body: data
+                })
+                .then(response => response.json())
+                .then( response => {
+                    console.log(response);
+                });
+        }
     }
 
+
     render() {
+
+        console.log(this.state);
         return (
             <Fragment>
                 <section className="section-bg bottom-padding-70">
@@ -34,24 +116,57 @@ class ContactComponent extends Component {
                             <label className="feedback-form__label">
                                 <span>Name<span>*</span></span>
                                 {/*feedback-form__input--error*/}
-                                <input type="text" id="feedback-name" placeholder="Your full name" name="fullname"
-                                       className="feedback-form__input"/>
-                                {/*<span className="feedback-form__error-msg">You full name</span>*/}
+                                <input type="text"
+                                       id="feedback-name"
+                                       placeholder="Your full name"
+                                       name="fullname"
+                                       className="feedback-form__input"
+                                       value={this.state.name}
+                                       onChange={(event) => {
+                                           this.setState({name: event.target.value})
+                                       }}
+                                />
+                                <span className="feedback-form__error-msg">Fill out the name</span>
                             </label>
                             <label className="feedback-form__label">
                                 <span> Email<span>*</span></span>
-                                <input type="text" id="feedback-email" placeholder="Work email address" name="email"
-                                       className="feedback-form__input"/>
+                                <input type="text"
+                                       id="feedback-email"
+                                       placeholder="Work email address"
+                                       name="email"
+                                       className="feedback-form__input"
+                                       value={this.state.email}
+                                       onChange={(event) => {
+                                           this.setState({email: event.target.value.toLowerCase()})
+                                       }}
+                                />
+                                <span className="feedback-form__error-msg">Fill out with valid email</span>
                             </label>
                             <label className="feedback-form__label">
                                 <span> Phone</span>
-                                <input type="text" id="feedback-phone" placeholder="Your phone number" name="phone"
-                                       className="feedback-form__input"/>
+                                <input type="text"
+                                       id="feedback-phone"
+                                       placeholder="Your phone number"
+                                       name="phone"
+                                       className="feedback-form__input"
+                                       value={this.state.phone}
+                                       onChange={(event) => {
+                                           this.setState({phone: event.target.value})
+                                       }}
+                                />
                             </label>
                             <label className="feedback-form__label feedback-form__label--textarea">
                                 <span>Message<span>*</span></span>
-                                <textarea id="feedback-message" placeholder="Tell us more about your project" name="description"
-                                          className="feedback-form__input"/>
+                                <textarea id="feedback-message"
+                                          placeholder="Tell us more about your project"
+                                          name="description"
+                                          className="feedback-form__input"
+                                          value={this.state.message}
+                                          onChange={(event) => {
+                                              this.setState({message: event.target.value})
+                                          }}
+                                />
+                                <span className="feedback-form__error-msg">Fill out the message</span>
                             </label>
                             <div className="form-btn-block">
                                 <button className="btn btn--160w" type="submit">Send Message</button>
